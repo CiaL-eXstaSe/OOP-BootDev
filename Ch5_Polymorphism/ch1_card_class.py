48170355009de921f60e92f090b96eea8052582b
CiaL-eXstaSe <j.l.vietinghoff@gmail.com>
2025-03-05 13:06:18 +0100

    challenge 1: card class - completed

--- Ch5_Polymorphism/ch1_card_class.py
+++ Ch5_Polymorphism/ch1_card_class.py
@@ -44,28 +44,34 @@ The .index list method is very useful when trying to determine the index
 of an element in a list.
 """
 
-Card Class
+import random
 
-We'll take a break from Age of Dragons to work on a simple deck of cards.
-Assignment
+SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
-Complete the Card class.
+RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
-    Define a constructor that takes rank and suit as parameters and sets rank, suit, rank_index, and suit_index instance variables.
 
-You will need the indexes of the ranks, and suits to help you compare them against each other. Keep in mind that a rank and a suit are just strings within a list.
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.rank_index = RANKS.index(rank)
+        self.suit_index = SUITS.index(suit)
 
-    Overload the following comparison operators:
+    def __eq__(self, other):
+        return self.rank_index == other.rank_index and self.suit_index == other.suit_index
 
-    ==: eq
-    >: gt
-    <: lt
+    def __lt__(self, other):
+        if self.rank_index == other.rank_index:
+            return self.suit_index < other.suit_index
+        return self.rank_index < other.rank_index
 
-Ranking the Cards
+    def __gt__(self, other):
+        if self.rank_index == other.rank_index:
+            return self.suit_index > other.suit_index
+        return self.rank_index > other.rank_index
 
-A card is "greater than" another card if it has a higher rank. However, if the ranks are the same, the card with the higher suit is "greater than" the other card. This same logic applies to the "less than" operator. The "equal to" operator should check that the rank AND suit are equal.
+    # don't touch below this line
 
-The suits and ranks are defined as global variables. The lower the index, the lower the rank or suit.
-Tips
-
-The .index list method is very useful when trying to determine the index of an element in a list.
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
